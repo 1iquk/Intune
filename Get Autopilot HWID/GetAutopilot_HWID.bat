45b89b08ae04d6c856524200241a8658402e3337
1iquk <iqclouduk@outlook.com>
2020-06-02 20:27:55 +0100

    edited batch comments

--- Get Autopilot HWID/GetAutopilot_HWID.bat
+++ Get Autopilot HWID/GetAutopilot_HWID.bat
@@ -1,9 +1,11 @@
 @ECHO ON
-:: Autopilot_HWID_1.5 Imran Qureshi, 23/05/2020
-:: This script starts required services then runs Get-WindowsAutoPilotInfo.ps1 v2.1
-:: It can be run from OOBE via USB or anywhere on the Windows desktop. 
-:: The csv file will be created if it doesn't exist. Each device's HW Hash will be added to the same csv file.
-:: Change the Group Tag to your own Tag or use Find/Replace All in a text editor later.
+GOTO :---
+Title/Author: Autopilot_HWID_1.5 Imran Qureshi, 23/05/2020
+Desc: This script starts required services then runs the Get-WindowsAutoPilotInfo.ps1 v2.1 script
+It can be run from OOBE via USB or anywhere on the Windows desktop. 
+The csv file will be created if it doesn't exist. Each device's HW Hash will be added to the same csv file.
+Change the Group Tag to your own Tag or use Find/Replace All in a text editor later.
+:---
 
 :: Start Windows Management Service
 net start WManSvc
