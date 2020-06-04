0ca173050a81f3dc1cd8c05bd29639b33eb72b23
1iquk <iqclouduk@outlook.com>
2020-06-04 23:01:48 +0100

    APAuto.bat Test

---
+++ Get Autopilot HWID/APAuto.bat
@@ -0,0 +1,11 @@
+
+
+curl -O "https://raw.githubusercontent.com/1iquk/Intune/master/Get%%20Autopilot%%20HWID/Get-WindowsAutoPilotInfo.ps1"
+curl -O "https://raw.githubusercontent.com/1iquk/Intune/master/Get%%20Autopilot%%20HWID/GetAutopilot_HWID.bat"
+
+CALL GetAutopilot_HWID.bat
+
+TIMEOUT 7
+
+Del Get-WindowsAutoPilotInfo.ps1
+Del GetAutopilot_HWID.bat
